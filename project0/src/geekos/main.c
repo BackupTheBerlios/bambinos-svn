e7967640ecb0078c7eaeb0ee9aecc774b53854c6
ruap <ruap@3eebf09c-9925-0410-b2b8-fc2346aa0d76>
2006-12-28 10:31:06 +0000

    RGR ADD completed project0

---
+++ project0/src/geekos/main.c
@@ -0,0 +1,77 @@
+/*
+ * GeekOS C code entry point
+ * Copyright (c) 2001,2003,2004 David H. Hovemeyer <daveho@cs.umd.edu>
+ * Copyright (c) 2003, Jeffrey K. Hollingsworth <hollings@cs.umd.edu>
+ * Copyright (c) 2004, Iulian Neamtiu <neamtiu@cs.umd.edu>
+ * $Revision: 1.51 $
+ * 
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "COPYING".
+ */
+
+#include <geekos/bootinfo.h>
+#include <geekos/string.h>
+#include <geekos/screen.h>
+#include <geekos/mem.h>
+#include <geekos/crc32.h>
+#include <geekos/tss.h>
+#include <geekos/int.h>
+#include <geekos/kthread.h>
+#include <geekos/trap.h>
+#include <geekos/timer.h>
+#include <geekos/keyboard.h>
+
+
+void Pressed_Key_Thread();
+
+/*
+ * Kernel C code entry point.
+ * Initializes kernel subsystems, mounts filesystems,
+ * and spawns init process.
+ */
+void Main(struct Boot_Info* bootInfo)
+{
+    Init_BSS();
+    Init_Screen();
+    Init_Mem(bootInfo);
+    Init_CRC32();
+    Init_TSS();
+    Init_Interrupts();
+    Init_Scheduler();
+    Init_Traps();
+    Init_Timer();
+    Init_Keyboard();
+
+
+    Set_Current_Attr(ATTRIB(BLACK, GREEN|BRIGHT));
+    Print("Welcome to GeekOS!\n");
+    Set_Current_Attr(ATTRIB(BLACK, GRAY));
+
+
+    //TODO("Start a kernel thread to echo pressed keys and print counts");
+	Start_Kernel_Thread(Pressed_Key_Thread,0, PRIORITY_NORMAL, true);
+	
+    /* Now this thread is done. */
+    Exit(0);
+}
+ 
+
+void Pressed_Key_Thread(){
+	Keycode kcode;
+	static int Count_Keys=0;
+	for (;;){
+		kcode=Wait_For_Key();
+		Print("pressed key: 0x%X \n",kcode);
+		Count_Keys++;
+		if (kcode==0x4064)
+			break;
+	}
+	Print("\n You pressed %d keys.",Count_Keys/2);
+}
+
+
+
+
+
+
+
