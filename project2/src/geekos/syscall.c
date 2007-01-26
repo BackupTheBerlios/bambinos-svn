b32ffd96a444a9be70a9b447fbc7964b784d2093
pizza_pete <pizza_pete@3eebf09c-9925-0410-b2b8-fc2346aa0d76>
2007-01-26 01:42:50 +0000

    project2 working

--- project2/src/geekos/syscall.c
+++ project2/src/geekos/syscall.c
@@ -46,7 +46,20 @@ static int Sys_Null(struct Interrupt_State* state)
  */
 static int Sys_Exit(struct Interrupt_State* state)
 {
-    TODO("Exit system call");
+	//lacki
+    //TODO("Exit system call");
+    int iExit_Code = state->ebx;
+    
+    
+    if (Interrupts_Enabled() == false) {
+    	Enable_Interrupts();
+    } 
+    
+    Detach_User_Context(g_currentThread);
+
+    // Exit frees kernel_thread memory
+    Exit(iExit_Code);
+    return 0;
 }
 
 /*
@@ -58,7 +71,36 @@ static int Sys_Exit(struct Interrupt_State* state)
  */
 static int Sys_PrintString(struct Interrupt_State* state)
 {
-    TODO("PrintString system call");
+	//lacki
+	unsigned int uNumber_Of_Chars = state->ecx;
+	int retval = 0;
+
+	if (uNumber_Of_Chars > 1024) {
+		Print("Error. Length of String exceeded 1024 chars");
+		return(-1);
+	}
+	
+	char *buffer = Malloc((uNumber_Of_Chars+1) * sizeof(char));
+	
+	if (buffer == 0) {
+		Print("Error: unable to allocate memory for the StringBuffer");
+	}
+	
+	
+	retval = Copy_From_User(buffer, state->ebx, uNumber_Of_Chars);
+	if (retval == false) {
+		Print("Error copying register-content to kernel-memory\n");
+	}
+	
+	buffer[uNumber_Of_Chars] = '\0';
+	//Print("size: %i\n", sizeof(buffer));
+	//Print("sizec: %u\n", uNumber_Of_Chars);
+	Print("%s", buffer);
+	Free(buffer);	
+    
+    return 0;
+
+    
 }
 
 /*
@@ -70,7 +112,12 @@ static int Sys_PrintString(struct Interrupt_State* state)
  */
 static int Sys_GetKey(struct Interrupt_State* state)
 {
-    TODO("GetKey system call");
+	//lacki
+	Keycode keycode;
+	keycode = Wait_For_Key();
+	
+	//Print ("keycode: %i", keycode);
+	return keycode;
 }
 
 /*
@@ -81,7 +128,10 @@ static int Sys_GetKey(struct Interrupt_State* state)
  */
 static int Sys_SetAttr(struct Interrupt_State* state)
 {
-    TODO("SetAttr system call");
+	//lacki
+    //TODO("SetAttr system call");
+	Set_Current_Attr(state->ebx);
+    return 0;
 }
 
 /*
@@ -93,7 +143,20 @@ static int Sys_SetAttr(struct Interrupt_State* state)
  */
 static int Sys_GetCursor(struct Interrupt_State* state)
 {
-    TODO("GetCursor system call");
+	//lacki
+    //TODO("GetCursor system call");
+    
+	int row; // = state->ebx;
+	int column; // = state->ecx;
+	
+	Get_Cursor(&row, &column);
+	
+	if (Copy_To_User(state->ebx, &row, sizeof(int)) & Copy_To_User(state->ecx, &column, sizeof(int))) {
+		return 0;
+	} else {
+		return -1;
+	}
+	
 }
 
 /*
@@ -105,7 +168,18 @@ static int Sys_GetCursor(struct Interrupt_State* state)
  */
 static int Sys_PutCursor(struct Interrupt_State* state)
 {
-    TODO("PutCursor system call");
+	//lacki
+    //TODO("PutCursor system call");
+	
+	int row = state->ebx;
+	int column = state->ecx;
+		
+    if(Put_Cursor(row, column)){
+    	return 0;
+    } else 
+    {
+    	return -1;
+    }
 }
 
 /*
@@ -119,7 +193,67 @@ static int Sys_PutCursor(struct Interrupt_State* state)
  */
 static int Sys_Spawn(struct Interrupt_State* state)
 {
-    TODO("Spawn system call");
+	//lacki
+    //TODO("Spawn system call");
+	
+	// name of program 
+    uint_t uExecutable_Name_Length = state->ecx;
+    char *pExecutable_Name = Malloc((uExecutable_Name_Length) * sizeof(char));
+    
+    Copy_From_User(pExecutable_Name, state->ebx, uExecutable_Name_Length);
+    pExecutable_Name[uExecutable_Name_Length] = '\0';
+ 
+/*
+    // display output
+    Print ("invoking program: \"");
+    int i;
+    for (i=0; i < uExecutable_Name_Length; i++) {
+    	Print("%c", pExecutable_Name[i]);
+    }
+    Print ("\"\n");
+    // ! display output
+*/ 
+    
+    // program plus args
+    uint_t uCommand_String_Length = state->esi;
+    if (uCommand_String_Length > VFS_MAX_PATH_LEN) {
+    	return (-1);
+    }
+    
+    char *pCommand_String = Malloc((uCommand_String_Length) * sizeof(char));
+    Copy_From_User(pCommand_String, state->edx, uCommand_String_Length);
+    pCommand_String[uCommand_String_Length] = '\0';
+    
+
+/*
+     Print ("l: %u\n", uCommand_String_Length);
+    // display output
+    Print ("invoking command: \"");
+
+    for (i=0; i < uCommand_String_Length; i++) {
+    	Print("%c", pCommand_String[i]);
+    }
+    Print ("\"\n");
+    // ! display output
+*/
+    
+    // spawns the executable and creates a usercontext
+    // retval is pid (> 0) or errorcode (< 0)
+    
+    if (Interrupts_Enabled() == false) {
+    	Enable_Interrupts();
+    }
+    
+    struct Kernel_Thread *kthread;
+    int retval = Spawn(pCommand_String, pExecutable_Name, &kthread);
+    
+    Disable_Interrupts();
+    
+    
+    Free(pCommand_String);
+    Free(pExecutable_Name);
+    
+    return retval;
 }
 
 /*
@@ -131,7 +265,32 @@ static int Sys_Spawn(struct Interrupt_State* state)
  */
 static int Sys_Wait(struct Interrupt_State* state)
 {
-    TODO("Wait system call");
+	// lacki
+    //TODO("Wait system call");
+	
+	uint_t pid = state->ebx;
+	uint_t uExit_Code = 0;
+
+
+    /* It is only legal for the owner to join */
+    //KASSERT(kthread->owner == g_currentThread);
+    
+    struct Kernel_Thread *kthread = Lookup_Thread(pid);
+  
+    if (kthread == 0) {
+    	Print("Interrupt SYS_WAIT: Not Thread with pid %u found.", pid);
+    	return (-1);
+    }
+    
+    
+    if (Interrupts_Enabled() == false) {
+    	Enable_Interrupts();	
+    }
+
+    uExit_Code = Join(kthread);
+    
+	return uExit_Code;
+	
 }
 
 /*
@@ -142,7 +301,8 @@ static int Sys_Wait(struct Interrupt_State* state)
  */
 static int Sys_GetPID(struct Interrupt_State* state)
 {
-    TODO("GetPID system call");
+    //TODO("GetPID system call");
+	return g_currentThread->pid;
 }
 
 
