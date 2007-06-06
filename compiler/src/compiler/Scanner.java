c03b9b9c7be77c7683be56e4980e6ecbc7cb4ad4
pizza_pete <pizza_pete@3eebf09c-9925-0410-b2b8-fc2346aa0d76>
2007-06-06 12:37:58 +0000

    alle Symbole werden erkannt

--- compiler/src/compiler/Scanner.java
+++ compiler/src/compiler/Scanner.java
@@ -5,6 +5,7 @@ import java.io.RandomAccessFile;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.EOFException;
+import java.util.Vector;
 
 
 
@@ -18,8 +19,9 @@ public class Scanner {
 	
 	private static Byte currentByte;
 	private static Byte nextByte;
+	// nextNextByte is the byte next to nextByte
+	private static Byte nextNextByte;
 	
-		
 	/**
 	 * Initialization for Scanner. It opens a filehandle 
 	 * @author lacki
@@ -58,10 +60,14 @@ public class Scanner {
 			readNextByte();
 			
 			if (currentByte == null) {
-				identifier = null;
+				identifier.type = TokenID.TEOF;
 				symbolFound = true;
 			// Numbers
 			} else if((currentByte > 47) && (currentByte < 58)) {
+				identifier = readNumberSymbol();
+				symbolFound = true;
+			// negative numbers
+			} else if((currentByte == 45) && (nextByte > 47) && (nextByte < 58)) {
 				identifier = readNumberSymbol();
 				symbolFound = true;
 			// text	(capital letter)
@@ -74,7 +80,15 @@ public class Scanner {
 				identifier = readTextSymbol();
 				symbolFound = true;
 				//System.out.println(identifier.getIdentValue() + " - " + identifier.type.toString());
-			// double quote
+			// double quote: start of a string-value
+			} else if(currentByte == 34) {
+				identifier = readStringValue();
+				symbolFound = true;
+			// single quote: start of a char-value
+			} else if(currentByte == 39) {
+				identifier = readCharValue();
+				symbolFound = true;
+			// space
 			} else if(currentByte == 32) {
 				//System.out.println("space - NOP");
 			// tabulator
@@ -87,6 +101,9 @@ public class Scanner {
 				passSinglelineComment();
 			} else if((currentByte == 47) &&  (nextByte == 42)) {
 				passMultilineComment();
+			} else if((currentByte == 46) && (charIsLetter(nextByte))) {
+					identifier = readTextSymbol();
+					symbolFound = true;
 			} else {
 				identifier = readSymbol();
 				//System.out.println("ident: " + ident.type.toString());
@@ -143,12 +160,6 @@ public class Scanner {
 				currentIdentifier.type = TokenID.TNOT;	
 			}
 			
-		// double quote
-		} else if(currentByte == 34) {
-			currentIdentifier.type = TokenID.TDQUOTE;
-		// single quote	
-		} else if(currentByte == 39) {
-			currentIdentifier.type = TokenID.TSQUOTE;
 		// ";"
 		} else if(currentByte == 59) {
 			currentIdentifier.type = TokenID.TSEMICOLON;
@@ -181,6 +192,12 @@ public class Scanner {
 		// "/"
 		} else if(currentByte == 47) {
 				currentIdentifier.type = TokenID.TDIV;
+		// "*"
+		} else if(currentByte == 42) {
+				currentIdentifier.type = TokenID.TMULT;
+		// "%"
+		} else if(currentByte == 37) {
+				currentIdentifier.type = TokenID.TMOD;
 		// ":"
 		} else if(currentByte == 58) {
 			currentIdentifier.type = TokenID.TCOLON;
@@ -228,46 +245,77 @@ public class Scanner {
 		return currentIdentifier;
 	}
 	
+	
 	/**
 	 *  Reads the next byte from the source-file. 
 	 */
 	public static void readNextByte() {
+		
 		try {
 			
 			if (currentByte == null) {
 				nextByte = file.readByte();
+				nextNextByte = file.readByte();
 			}
+				
 
 			currentByte = nextByte;
-			nextByte = file.readByte();
+			nextByte = nextNextByte;
+			nextNextByte = file.readByte();
 		} catch(EOFException eof) {
-			nextByte = null;
+			nextNextByte = null;
 		} catch(IOException io) {
 			System.out.println("Error reading file: " + io.toString());	
-		} 
+		}
+		
 	}
 	
 	/**
-	 * Analyzes a symbol, that starts with a digit != 0. 
-	 * This results in a number
-	 * @return Ident
+	 * Analyzes a symbol, that starts with a digit != 0. If the symbol does not end with a correct follow-symbol (look at ebnf)
+	 * it returns an error-symbol. Otherwise it's a number-symbol
+	 * @return TNUmber
 	 */
 	public static Ident readNumberSymbol() {
 		
 		Ident currentIdentifier = new Ident();
 		boolean endOfSymbol = false;
+		boolean errorSymbol = true;
+		String symbolValue = new String();
+		
+		byte[] followSymbol = {32, 33, 37, 38, 41, 42, 43, 44, 45, 47, 59, 60, 61, 62, 93, 124};
+		
+		
 		
 		while (endOfSymbol == false) {
 			
-			if (charIsDigit(nextByte)) {
-				readNextByte();
-			} else {
-				endOfSymbol = true;
-			}
+			symbolValue = symbolValue + (char)(int)currentByte;
 			
+			if (charIsDigit(nextByte)) {
+				
+				readNextByte();
+				
+			} else {
+			
+				//// checks if the nextByte is part of the followSymbols
+				for (int i = 0; i < followSymbol.length; i++) {
+					if (nextByte == followSymbol[i]) {
+						errorSymbol = false;
+						endOfSymbol = true;
+					} else {
+						endOfSymbol = true;
+					}
+				}
+				
+			}
+		}
+			
+		if (errorSymbol == false) {
+			currentIdentifier.type = TokenID.TNUMBER;	
+			currentIdentifier.setIdentValue(symbolValue);
+		} else {
+			currentIdentifier.type = TokenID.TERROR;
 		}
 		
-		currentIdentifier.type = TokenID.TIDENT;
 		
 		return currentIdentifier;
 	}
@@ -283,9 +331,28 @@ public class Scanner {
 		
 		String symbolValue = new String();
 		boolean endOfSymbol = false;
+		boolean errorSymbol = false;
 		boolean symbolIsIdentifier = false;
-		
 
+		/* tests if the simpleIdentifier starts with a "."
+		 * then we need to check if the next Symbol is a letter
+		*/
+		if (currentByte == 46) {
+			symbolValue = symbolValue + (char)(int)currentByte;
+			
+			// if it's no letter, don't check for identifier but return a TDOT-Symbol
+			if (charIsLetter(nextByte) == false) {
+				endOfSymbol = true;
+				currentIdentifier.type = TokenID.TDOT;
+			} else {
+				readNextByte();
+				symbolIsIdentifier = true;
+			}
+				
+		}
+		
+		// followSymbol definies the available symbols that can follow the identifier
+		byte[] followSymbols = {61, 60, 62, 33, 32, 38, 124, 59, 40, 41, 91, 93, 44, 42, 47, 37, 43, 45};
 		
 		while (endOfSymbol == false) {
 
@@ -299,21 +366,109 @@ public class Scanner {
 				symbolIsIdentifier = true;
 				readNextByte();
 				
-			// if nextByte is a dot (47), the symbol must be a identifier and not a reserved symbol
+			// if nextByte is a dot (46), the symbol must be a identifier and not a reserved symbol
 			} else if (nextByte == 46) {
 				symbolIsIdentifier = true;
-				readNextByte();			
-			
+				endOfSymbol = true;
+				//readNextByte();			
+			// if nextByte is a bracket (91), the symbol must be a identifier and not a reserved symbol
+			} else if ((nextByte == 91) && (nextNextByte == 93)){
+				symbolIsIdentifier = false;
+				endOfSymbol = true;
+				readNextByte();		
+				symbolValue = symbolValue + (char)(int)currentByte;
+				readNextByte();
+				symbolValue = symbolValue + (char)(int)currentByte;
 			// if the nextByte is not one of the above, then the symbol ends
 			} else {
+				
+				boolean followSymbolFound = false;
+				//// checks if the nextByte is part of the followSymbols
+				for (int i = 0; i < followSymbols.length; i++) {
+					if (nextByte == followSymbols[i]) {
+						followSymbolFound = true;
+					}
+				}
+				
+				if (followSymbolFound == false) {
+					errorSymbol = true;
+				} 
+				
 				endOfSymbol = true;
 			}
 				
 
 		}
+
+		if (errorSymbol == true) {
+			currentIdentifier.type = TokenID.TERROR;
+		} else {
+			
+			if (symbolIsIdentifier = false) {
+				currentIdentifier.type = TokenID.TSIDENT;
+			} else {
+				currentIdentifier = getIdentifier(symbolValue);	
+			}			
+			
+		}
 		
-		currentIdentifier = getIdentifier(symbolValue);
+		currentIdentifier.setIdentValue(symbolValue);
+		
 		return currentIdentifier;
+	}
+	
+	/**
+	 * Reads a text until the end-symbol " is reached
+	 * @return Ident
+	 */
+	public static Ident readStringValue() {
+		
+		Ident currentIdentifier = new Ident();
+		String symbolValue = new String();
+
+		// read NextByte because the currentByteValue is "
+		readNextByte();
+		while (currentByte != 34) {
+			
+			symbolValue = symbolValue + (char)(int)currentByte;
+			readNextByte();
+		}
+		
+		currentIdentifier.type = TokenID.TSTRING_VALUE;
+		currentIdentifier.setIdentValue(symbolValue);
+		
+		return currentIdentifier;
+	}
+	
+	/**
+	 * Reads a text until the end-symbol ' is reached
+	 * The text must only be 1 symbol. Else an error is returned
+	 * @return Ident
+	 */
+	public static Ident readCharValue() {
+		
+		Ident currentIdentifier = new Ident();
+		
+		// read NextByte because the currentByteValue is '
+		readNextByte();
+		
+		// the currentByteValue is the value of the char and the next ByteValue must be '
+		// otherwise an error is generated. 
+		
+		if (currentByte == 39) {
+			currentIdentifier.type = TokenID.TCHAR_VALUE;
+			currentIdentifier.setIdentValue("");
+		} else if (nextByte == 39) {
+			currentIdentifier.type = TokenID.TCHAR_VALUE;
+			currentIdentifier.setIdentValue("" + (char)(int)currentByte);
+			readNextByte();
+		} else {
+			currentIdentifier.type = TokenID.TERROR;
+			currentIdentifier.setIdentValue("");
+		}
+		
+		return currentIdentifier;
+		
 	}
 	
 	/**
@@ -340,20 +495,40 @@ public class Scanner {
 			identifier.type = TokenID.TFINAL;
 		} else if (symbolValue.compareTo("return") == 0) {
 			identifier.type = TokenID.TRETURN;
+		} else if (symbolValue.compareTo("void") == 0) {
+			identifier.type = TokenID.TVOID;
 		} else if (symbolValue.compareTo("null") == 0) {
 			identifier.type = TokenID.TNULL;
 		} else if (symbolValue.compareTo("new") == 0) {
 			identifier.type = TokenID.TNEW;
 		} else if (symbolValue.compareTo("if") == 0) {
 			identifier.type = TokenID.TIF;
-		} else if (symbolValue.compareTo("then") == 0) {
-			identifier.type = TokenID.TELSE;
 		} else if (symbolValue.compareTo("else") == 0) {
 			identifier.type = TokenID.TELSE;
 		} else if (symbolValue.compareTo("while") == 0) {
 			identifier.type = TokenID.TWHILE;
+		} else if (symbolValue.compareTo("int") == 0) {
+			identifier.type = TokenID.TINT;
+		} else if (symbolValue.compareTo("int[]") == 0) {
+			identifier.type = TokenID.TINT_ARRAY;
+		} else if (symbolValue.compareTo("boolean") == 0) {
+			identifier.type = TokenID.TBOOL;
+		} else if (symbolValue.compareTo("bool[]") == 0) {
+			identifier.type = TokenID.TBOOL_ARRAY;
+		} else if (symbolValue.compareTo("char") == 0) {
+			identifier.type = TokenID.TCHAR;
+		} else if (symbolValue.compareTo("char[]") == 0) {
+			identifier.type = TokenID.TCHAR_ARRAY;
+		} else if (symbolValue.compareTo("String") == 0) {
+			identifier.type = TokenID.TSTRING;
+		} else if (symbolValue.compareTo("String[]") == 0) {
+			identifier.type = TokenID.TSTRING_ARRAY;
+		} else if (symbolValue.compareTo("true") == 0) {
+			identifier.type = TokenID.TTRUE;
+		} else if (symbolValue.compareTo("false") == 0) {
+			identifier.type = TokenID.TFALSE;
 		} else {
-			identifier.type = TokenID.TIDENT;
+			identifier.type = TokenID.TSIDENT;
 		}
 		
 		return identifier;
