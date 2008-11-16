838d6e09b52252151236be294e08fe1dae839ba9
infinum <infinum@3eebf09c-9925-0410-b2b8-fc2346aa0d76>
2008-11-16 20:38:32 +0000

    pimped producer

--- kMod01/producer.c
+++ kMod01/producer.c
@@ -4,37 +4,51 @@
 
 
 int main(int argc, char *argv[]) {
-	
+
 	FILE *fh;
-	char msg[256];
-	struct timespec tm, tm2;	
-	int sleep_ms;
+	char *m;
+
+	struct timespec tm, tm2;
+	int nr_msg, sleep_ms;
 	int i, count;
-	
+
+	m = "message";
+	nr_msg = 666;
 	sleep_ms = 100;
+
 	if (argc>1) {
-		sleep_ms = atoi(argv[1]);
-	
+		m = argv[1];
 	}else {
-		printf("no period specified, using %d ms as default\n", sleep_ms); 
-	}		
-	
+		printf("NOTE: no message specified, using '%s' as default.\n", m);
+	}
+
+	if (argc>2) {
+		nr_msg = atoi(argv[2]);
+	}else {
+		printf("NOTE: number of messages not specified, using %i as default.\n", nr_msg);
+	}
+
+	if (argc>3) {
+		sleep_ms = atoi(argv[3]);
+	}else {
+		printf("NOTE: no period specified, using %i ms as default.\n", sleep_ms);
+	}
+
    	tm.tv_sec = 0;
    	tm.tv_nsec = 1000000 * sleep_ms;
 
 	fh = fowrite("/dev/mbcd");
 	i = 0;
-	
-	while(1) {	
-		
-		sprintf(msg, "message %i", i);
-		printf("writing: %s\n", msg);
-		fwrite(msg, 1, sizeof(msg), fh); 	
-	
+
+	while(i < nr_msg) {
+
+		printf("writing message %i: %s\n", i, m);
+		fwrite(m, 1, sizeof(m), fh);
+
 		nanosleep(&tm, &tm2);
 		i++;
 	}
-	
+
 	fclose(fh);
 
 	return 0;
