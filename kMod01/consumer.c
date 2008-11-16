f8a87a913b0dee60cc7c451ab0990225746b90a5
infinum <infinum@3eebf09c-9925-0410-b2b8-fc2346aa0d76>
2008-11-16 21:21:27 +0000

    minor changes

--- kMod01/consumer.c
+++ kMod01/consumer.c
@@ -3,36 +3,40 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
-	
+
 	FILE *fh;
-	char msg[256];
-	struct timespec tm, tm2;	
+	char msg[20];
+	char *m;
+	struct timespec tm, tm2;
 	int sleep_ms;
 	int i, count;
-	
+
 	sleep_ms = 100;
 	if (argc>1) {
 		sleep_ms = atoi(argv[1]);
-	
+
 	}else {
-		printf("no period specified, using %d ms as default\n", sleep_ms); 
-	}		
-	
+		printf("NOTE: no period specified, using %d ms as default\n", sleep_ms);
+	}
+
    	tm.tv_sec = 0;
    	tm.tv_nsec = 1000000 * sleep_ms;
 
-	fh = foread("/dev/mbcd");
+	fh = foread("/dev/mbcdd");
+   	//fh = foread(argv[2]);
 	i = 0;
-	
-	while(1) {			
-		
-		fread(msg, 1, sizeof(msg), fh); 	
-		printf("read #%i: %s\n", i, msg);
-		
+
+	while(!feof(fh)) { // || !feof(fh)
+
+		fread(msg, 1, sizeof(msg), fh);
+
+		//printf("read %i: %s\n", i, msg);
+		printf("%s", msg);
+
 		nanosleep(&tm, &tm2);
 		i++;
 	}
-	
+
 	fclose(fh);
 
 	return 0;
