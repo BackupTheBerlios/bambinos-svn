ccb88b51fafb0102cf7a45d2750fbb53b65f19b1
ruap <ruap@3eebf09c-9925-0410-b2b8-fc2346aa0d76>
2009-01-04 16:47:25 +0000

    RGR change comments ansi conform

--- minix/src/lib/posix/scheduler.c
+++ minix/src/lib/posix/scheduler.c
@@ -16,8 +16,8 @@ int sched_getscheduler(pid_t pid)
 	int v;
 	message m;
 
-	//m.m1_i1 = which;
-	m.m1_i2 = pid; //who
+	/* m.m1_i1 = which; */
+	m.m1_i2 = pid; /* who */
 
 	return _syscall(MM, GETSCHEDULER, &m);
 
@@ -32,6 +32,6 @@ int sched_setscheduler(pid_t pid, int policy, int priority)
 	m.m1_i3 = priority;
 
 	return _syscall(MM, SETSCHEDULER, &m);
-	//return _syscall(MM, SETPRIORITY, &m);
+	/* return _syscall(MM, SETPRIORITY, &m); */
 }
 
