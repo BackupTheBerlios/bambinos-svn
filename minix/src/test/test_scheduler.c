fd369976938e244773cb24cdca58d2f935c0e02e
infinum <infinum@3eebf09c-9925-0410-b2b8-fc2346aa0d76>
2009-01-09 17:40:06 +0000

    add 2nd parameter schedule

--- minix/src/test/test_scheduler.c
+++ minix/src/test/test_scheduler.c
@@ -19,10 +19,16 @@ void main(int argc, char argv[]) {
 
 	int rs, rg;
 	pid_t pid = 0;
+	int sched = SCHED_RR;
+
 	if (argc > 1)
 		pid =  atoi(argv[1]);
 
-	rs = sched_setscheduler(pid,2,0);
+	if (argc > 2)
+		sched =  atoi(argv[2]);
+
+
+	rs = sched_setscheduler(pid,sched,0);
 
 	rg = sched_getscheduler(pid);
 	printf("r set:%d get:%d \n", rs, rg);
